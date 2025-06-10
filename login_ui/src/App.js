be3e9212137d543046724b63416c7a2cf6105248
Retread2000 <153476084+Retread2000@users.noreply.github.com>
2025-06-09 23:13:01 -0400

    Department Selection UI
    -Added basic department selection UI popup.
    -Testing popup for this purpose, potentially change to normal web page.
    -Edited App.js to test function of popup.

--- login_ui/src/App.js
+++ login_ui/src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-import Login from './Components/Login/Login';
+import DepartmentSelection from './Components/DepartmentSelection/DepartmentSelection';
 
 function App() {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
   return (
     <div> 
-      <Login/>
+      <h1>Departments</h1>
+      <button onClick={() => setIsPopupOpen(true)}>Open Popup</button>
+      <DepartmentSelection 
+        trigger={isPopupOpen} 
+        setTrigger={setIsPopupOpen}
+      >
+        <h3>My Popup</h3>
+      </DepartmentSelection>
     </div>
   );
 }
